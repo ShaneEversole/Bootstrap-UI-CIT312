c341c0cb966b49bd6a23c648848ae6af58875aff
ShaneEversole <eversoleshane7@gmail.com>
2026-04-13 13:05:15 -0400

    Add files via upload

---
+++ enabler.js
@@ -0,0 +1,15 @@
+
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (el) {
+  return new bootstrap.Popover(el);
+});
+
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (el) {
+  return new bootstrap.Tooltip(el);
+});
